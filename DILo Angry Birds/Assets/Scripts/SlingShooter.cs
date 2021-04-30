db000a098a9befdf6f7a7d34f25c8baaf7b01456
kizaruu77 <dionovan7@gmail.com>
2021-04-30 09:22:39 +0700

    scripting bird

---
+++ DILo Angry Birds/Assets/Scripts/SlingShooter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShooter : MonoBehaviour
+{
+    public CircleCollider2D Collider;
+    private Vector2 _startPos;
+
+    [SerializeField]
+    private float _radius = 0.75f;
+
+    [SerializeField]
+    private float _throwSpeed = 30f;
+
+    private void Start()
+    {
+        _startPos = transform.position;
+    }
+
+    private void OnMouseUp()
+    {
+        Collider.enabled = false;
+        Vector2 velocity = _startPos - (Vector2)transform.position;
+        float distance = Vector2.Distance(_startPos, transform.position);
+
+        gameObject.transform.position = _startPos;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dir = p - _startPos;
+        if (dir.sqrMagnitude > _radius)
+            dir = dir.normalized * _radius;
+        transform.position = _startPos + dir;
+    }
+}
